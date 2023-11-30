3b116eac6ab205d283ba5834113bdca8f7ceabd9
Mellody <mellodynguyen@outlook.com>
2023-11-29 20:48:51 -0600

    Guessing Game

--- game.py
+++ game.py
@@ -17,6 +17,8 @@
 import random
 
 def random_number_game():
+    """A number-guessing game."""
+    
     random_number = random.randint(1,100)
     guess = None
     number_of_guesses = 0
@@ -29,7 +31,18 @@ def random_number_game():
     name = input("type in your name: ")
     print(f"{name}, im thinking of a number between 1 and 100.")
     print("Try to guess my number")
-    guess = int(input("Your guess? "))
+
+# 8.3 handling exceptions 
+# https://docs.python.org/3/tutorial/errors.html
+
+# ensuring the user inputs an integer and 
+    while True:
+        try:
+            guess = int(input("Your guess? "))
+            break
+        except ValueError:
+            print("That is not a valid number, please enter a number.")
+        
     while guess != random_number: 
         if guess < random_number:
             print("Your guess is too low, try again.")
